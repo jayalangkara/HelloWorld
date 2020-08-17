2f1aef07c186470bbb94c4351fed6279ea4553eb
jayalangkara <tuan.p.jayalangkara@gmail.com>
2020-08-17 17:50:42 +0700

    First Sync

--- HelloWorld/Startup.cs
+++ HelloWorld/Startup.cs
@@ -27,7 +27,7 @@ namespace HelloWorld
 
       app.Run(async (context) =>
       {
-        await context.Response.WriteAsync("Hello World!");
+        await context.Response.WriteAsync("Hello World! Hello ASP.NET Core \n");
       });
     }
   }
